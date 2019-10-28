42bc2cbeca845f02bcd5cbb4825530641bf77da6
Goglow <waggle.glow@gmail.com>
2019-10-28 13:21:22 -0400

    Update different files

---
+++ OpenQuizz/OpenQuizz/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Melissa GS on 07/10/2019.
+//  Copyright © 2019 Goglow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
